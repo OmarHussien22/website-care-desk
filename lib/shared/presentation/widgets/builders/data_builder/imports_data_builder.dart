92fa2e545b5dfb2c33c99196394b6ba5aa081a1c
Ahmed Arafat <ahmed.arafat3535@gmail.com>
2026-04-26 16:18:49 +0300

    fix conflicts

---
+++ lib/shared/presentation/widgets/builders/data_builder/imports_data_builder.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+import '../../../../../core/network_structure/resources/data_state/data_state.dart';
+import '../../../../../core/network_structure/resources/errors/error_model.dart';
+import '../../../../../core/services/utils/snack_utils/src/app_snacks.dart';
+import '../../../../../core/services/utils/snack_utils/src/constants/snack_constants.dart';
+import '../../../../../core/utils/general_utils.dart';
+import '../../error/simple_error_handler_builder.dart';
+import '../../loading/app_loading.dart';
+
+part 'button_data_status_builder.dart';
+part 'data_failed_builder.dart';
+part 'data_status_builder.dart';
